1163c16baaf6717dcd062cd44b9813e1b0357b20
Kus Cámara <kcmr@users.noreply.github.com>
2019-02-05 18:30:32 +0100

    feat(bundler): allow to config autoprefixer options
    autoprefixer options use app targets by default or addon options

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,7 +8,11 @@ module.exports = function(defaults) {
 	const app = new EmberAddon(defaults, {
 		'ember-cli-polymer-bundler': {
 			htmlImportsFile: path.join('tests', 'dummy', 'app', 'elements.html'),
-			autoprefixStyles: true
+			autoprefixer: {
+				browsers: ['chrome >= 30', 'firefox >= 32', 'ios >= 9', 'last 1 edge versions'],
+				enabled: true,
+				cascade: false
+			}
 		}
 	});
 
