0613e2f017ec33c2c9bd66cc07fbb25fc3456810
Kus Cámara <kcmr@users.noreply.github.com>
2019-03-27 22:07:10 +0100

    test(addon): update default ember-cli-build

--- node-tests/addon/ember-cli-build-default.js
+++ node-tests/addon/ember-cli-build-default.js
@@ -7,7 +7,12 @@ const path = require('path');
 module.exports = function(defaults) {
 	const app = new EmberAddon(defaults, {
 		'ember-cli-polymer-bundler': {
-			htmlImportsFile: path.join('tests', 'dummy', 'app', 'elements.html')
+			htmlImportsFile: path.join('tests', 'dummy', 'app', 'elements.html'),
+			autoprefixer: {
+				browsers: ['chrome >= 30', 'firefox >= 32', 'ios >= 9', 'last 1 edge versions'],
+				enabled: true,
+				cascade: false
+			}
 		}
 	});
 
