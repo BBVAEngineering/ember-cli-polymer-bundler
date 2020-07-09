8420dd61964938c83615b48970bf67b766cdc8bf
Kus Cámara <kcmr@users.noreply.github.com>
2020-07-09 11:17:10 +0200

    feat: rebuild bundle only on changes
    BREAKING CHANGE: web components are not looked for inside node_modules any more
    
    chore(vscode): remove folder
    
    feat: use caching-writer to prevent unneeded rebuilds
    
    BREAKING CHANGE: web components are not looked for inside node_modules any more (assuming using Polymer & Bower)
    
    style: change eslint settings and remove eslint file comments
    
    refactor: rename property
    
    test: remove skipped tests for not implemented functionality
    
    test: increase coverage thresholds
    
    docs(readme): document new property and changes

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,4 +1,3 @@
-/* eslint-env node */
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
@@ -8,11 +7,9 @@ module.exports = function(defaults) {
 	const app = new EmberAddon(defaults, {
 		'ember-cli-polymer-bundler': {
 			htmlImportsFile: path.join('tests', 'dummy', 'app', 'elements.html'),
-			autoprefixer: {
-				browsers: ['chrome >= 30', 'firefox >= 32', 'ios >= 9', 'last 1 edge versions'],
-				enabled: true,
-				cascade: false
-			}
+			elementPaths: [
+				'tests/dummy/app/elements'
+			]
 		}
 	});
 
